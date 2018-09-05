ae468648d76c2fe9f8497955d6f32af05ef712d5
admin <admin@17b>
2018-09-05 12:32:17 +0700

    ver 0.0

---
+++ src/testview/testdialog1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testview;
+
+import view.DaftarAntrianDialog;
+
+/**
+ *
+ * @author admin
+ */
+public class testdialog1 {
+      public static void main(String[] args) {
+        DaftarAntrianDialog  test = new DaftarAntrianDialog ("latihan");
+        test.setSize(300, 400);       
+        test.setVisible(true);
+}
+}
