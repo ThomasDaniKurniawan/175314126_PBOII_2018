90c8bd1b4ae42dadbfe6d3c19aa9e58715b7bfe2
Lenovo <Lenovo@LAPTOP-8VJDIM5C>
2018-09-16 18:48:01 +0700

    16 sept 18

--- src/view/DaftarAntrianDialog.java
+++ src/view/DaftarAntrianDialog.java
@@ -5,18 +5,32 @@
  */
 package view;
 
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 /**
  *
- * @author admin
+ * @author Tom
  */
 public class DaftarAntrianDialog extends JDialog {
     private JLabel JudulLabel;
     private JLabel namaLabel;
-    private JLabel noRekamMedis;
-    private JLabel alamat;
+    private JLabel noRekamMedisLabel;
+    private JLabel tgllabel;
+    private JComboBox tgl;
+    private JLabel blnlabel;
+    private JComboBox bln;
+    private JLabel thnlabel;
+    private JComboBox thn;
+    private JRadioButton daftarbutton;
+    private JButton daftar;
+
+    private JTextField noRekamMedistext;
     
    public DaftarAntrianDialog() {
         init();
@@ -32,24 +46,84 @@ public class DaftarAntrianDialog extends JDialog {
         
         
         
-        JudulLabel = new JLabel("DAFTAR PASIEN");
+        JudulLabel = new JLabel("DAFTAR ANTRIAN PASIEN");
         JudulLabel.setBounds(50, 10, 150, 10);
         this.add( JudulLabel);
         
-        noRekamMedis = new JLabel("NoRM");
-        noRekamMedis.setBounds(50, 50, 50, 10);
-        this.add(noRekamMedis);
+        noRekamMedisLabel = new JLabel("NoRM");
+        noRekamMedisLabel.setFont(new Font(null,Font.BOLD,14)); 
+        noRekamMedisLabel.setBounds(50, 70, 50, 30);
+        this.add(noRekamMedisLabel);
         
-        namaLabel = new JLabel("Nama");
-        namaLabel.setBounds(50, 70, 50, 10);
-        this.add(namaLabel);
+        JudulLabel = new JLabel("TANGGAL LAHIR :");
+        JudulLabel.setBounds(50, 110, 150, 30);
+        this.add( JudulLabel);
         
-        alamat = new JLabel("alamat");
-        alamat.setBounds(50, 90, 50, 10);
-        this.add( alamat);
+        this.setLayout(null);
+        noRekamMedistext = new JTextField();
+        noRekamMedistext.setBounds(130, 70, 100, 30);
+        noRekamMedistext.setFont(new Font(null,Font.PLAIN,18));
+        this.add(noRekamMedistext);
         
+        this.setLayout(null);
+        tgllabel = new JLabel("TANGGAL");
+        tgllabel.setFont(new Font(null,Font.BOLD,14)); 
+        tgllabel.setBounds(50, 130, 100, 50);
+        this.add(tgllabel);
+                  
+        this.setLayout(null);
+        blnlabel = new JLabel("BULAN");
+        blnlabel.setFont(new Font(null,Font.BOLD,14)); 
+        blnlabel.setBounds(50, 170, 100, 50);
+        this.add(blnlabel);        
+           
+        this.setLayout(null);
+        thnlabel = new JLabel("TAHUN");
+        thnlabel.setFont(new Font(null,Font.BOLD,14)); 
+        thnlabel.setBounds(50, 220, 100, 30);
+        this.add(thnlabel);
         
+        this.setLayout(null);
+        tgl = new JComboBox();
+        for (int i = 1; i <=31; i++) {
+        tgl.addItem(i);
+        }
+        tgl.setBounds(130, 140, 100, 30);
+        this.add(tgl);
         
+        this.setLayout(null);
+        bln = new JComboBox();
+        bln.addItem("January");
+        bln.addItem("February");
+        bln.addItem("March");
+        bln.addItem("April");
+        bln.addItem("May");
+        bln.addItem("June");
+        bln.addItem("July");
+        bln.addItem("august");
+        bln.addItem("September");
+        bln.addItem("October");
+        bln.addItem("November");
+        bln.addItem("December");
+        bln.setBounds(130, 180, 100, 30);
+        this.add(bln);
+        
+        this.setLayout(null);
+        thn = new JComboBox();
+        for (int i = 1990; i <=2018; i++) {
+        thn.addItem(i);
+        }
+        thn.setBounds(130, 220, 100, 30);
+        this.add(thn);
+
+         
+        this.setLayout(null);
+        daftar = new JButton("daftar");
+        daftar.setBounds(100, 300, 100, 30);
+        daftar.setFont(new Font(null,Font.BOLD,20));   
+        this.add(daftar);
+        
+                
         
     }
 }
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -6,13 +6,14 @@
 package view;
 
 import java.awt.HeadlessException;
-import java.awt.MenuBar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+
+
 
 /**
  *
@@ -23,13 +24,15 @@ public class MainFrame extends JFrame implements ActionListener {
     private JMenuBar menuBar;
     private JMenu fileMenu;
     private JMenuItem exitMenuItem;
-    private JMenuItem tambahPasienMenuItem;
+    private JMenuItem TambahPasienBaruDialog;
+    private JMenuItem DaftarAntrianDialog;
 
     public MainFrame() throws HeadlessException {
         init();
     }
 
     public void init() {
+        
         // buat menu bar
         menuBar = new JMenuBar();
         // set Titile
@@ -37,13 +40,17 @@ public class MainFrame extends JFrame implements ActionListener {
         // buat menu
         fileMenu = new JMenu("File");
         exitMenuItem = new JMenuItem("exit");
-        tambahPasienMenuItem = new JMenuItem("TambahPasien");
-        fileMenu.add(tambahPasienMenuItem);
+        TambahPasienBaruDialog = new JMenuItem("TambahPasien");
+        DaftarAntrianDialog = new JMenuItem("Tambah Antrian");
+        
+        fileMenu.add(TambahPasienBaruDialog);
+        fileMenu.add(DaftarAntrianDialog);
         fileMenu.add(exitMenuItem);
         menuBar.add(fileMenu);
 
         exitMenuItem.addActionListener(this);
-        tambahPasienMenuItem.addActionListener(this);
+        TambahPasienBaruDialog.addActionListener(this); // actionlistener1
+        DaftarAntrianDialog.addActionListener(this);
 
         this.setJMenuBar(menuBar);
     }
@@ -53,11 +60,17 @@ public class MainFrame extends JFrame implements ActionListener {
         if (e.getSource() == exitMenuItem) {
             System.exit(0);
         }
-        if (e.getSource() == tambahPasienMenuItem) {
+        if (e.getSource() == TambahPasienBaruDialog) {
             // nantinya diganti TambahPasienBaruDialog
-            LatihanDialog test = new LatihanDialog();
-            test.setSize(300, 400);
+            TambahPasienBaruDialog test = new TambahPasienBaruDialog();
+            test.setSize(500, 400);
             test.setVisible(true);
+        }
+        if (e.getSource() == DaftarAntrianDialog) {
+            DaftarAntrianDialog daftar = new DaftarAntrianDialog();
+            daftar.setSize(500,400);
+            daftar.setVisible(true);
+            
         }
     }
 
